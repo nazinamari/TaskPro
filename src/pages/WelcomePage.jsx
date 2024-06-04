29cdaedae7c51259f3f30a9166c6d3c35b2f3f9f
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-04 23:20:47 +0100

    vercel

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import Title from '../components/Title/Title';
 
 export default function WelcomePage() {
 	return (
 		<div>
 			<Title>WelcomePage</Title>
+			<Link to="/auth/login">Log In</Link>
+			<Link to="/auth/register">Register</Link>
 		</div>
 	);
 }
