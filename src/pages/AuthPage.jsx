539fca20783dbbdb492a573ded88909f5a52b9d4
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-06 16:29:17 +0300

    bug fixed in routes

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,11 +1,13 @@
+import { Outlet } from "react-router-dom";
 import LoginForm from "../components/LoginForm/LoginForm";
-import Title from '../shared/Title/Title';
-
+import RegisterForm from "../components/RegisterForm/RegisterForm";
 
 export default function AuthPage() {
   return (
     <div>
+      <Outlet />
       <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
