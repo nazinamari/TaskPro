a1b419950b82043d8843e02487a36e4c534ec6c9
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-06 23:36:50 +0300

    add loginForm and registerForm

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import styled from "./RegisterForm.module.css";
 
 const buildLinkClass = ({ isActive }) => {
-  return clsx(styled.loginLink, isActive && styled.linkActive);
+  return clsx(styled.registerLink, isActive && styled.linkActive);
 };
 
 export default function RegisterForm() {
@@ -26,49 +26,56 @@ export default function RegisterForm() {
   console.log(errors, formState);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <ul>
-          <li>
-            <NavLink to="/" className={buildLinkClass}>
-              Registration
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="login" className={buildLinkClass}>
-              Log In
-            </NavLink>
-          </li>
-        </ul>
+    <div className={styled.registerContainer}>
+      <div className={styled.registerWrapper}>
+        <form className={styled.formWrapper} onSubmit={handleSubmit(onSubmit)}>
+          <ul className={styled.registerList}>
+            <li className={styled.registerItem}>
+              <NavLink to="register" className={buildLinkClass}>
+                Registration
+              </NavLink>
+            </li>
+            <li className={styled.registerItem}>
+              <NavLink to="login" className={buildLinkClass}>
+                Log In
+              </NavLink>
+            </li>
+          </ul>
 
-        <input
-          type="text"
-          placeholder="Enter your name"
-          {...register("name", {
-            required: "The name field is not filled",
-          })}
-        />
-        {errors.email && <div>{errors.email.message}</div>}
+          <input
+            className={styled.registerInput}
+            type="text"
+            placeholder="Enter your name"
+            {...register("name", {
+              required: "The name field is not filled",
+            })}
+          />
+          {errors.email && <div>{errors.email.message}</div>}
 
-        <input
-          type="text"
-          placeholder="Enter your email"
-          {...register("email", {
-            required: "The email field is not filled",
-          })}
-        />
-        {errors.email && <div>{errors.email.message}</div>}
+          <input
+            className={styled.registerInput}
+            type="text"
+            placeholder="Enter your email"
+            {...register("email", {
+              required: "The email field is not filled",
+            })}
+          />
+          {errors.email && <div>{errors.email.message}</div>}
 
-        <input
-          type="password"
-          placeholder="Confirm a password"
-          {...register("password", {
-            required: "The password field is not filled",
-          })}
-        />
+          <input
+            className={styled.registerInput}
+            type="password"
+            placeholder="Confirm a password"
+            {...register("password", {
+              required: "The password field is not filled",
+            })}
+          />
 
-        <button type="submit">Register Now</button>
-      </form>
+          <button className={styled.registerButton} type="submit">
+            Register Now
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
