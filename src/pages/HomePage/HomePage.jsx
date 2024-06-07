84672a2e2c4ed582966497d9b45fae5e0b0a05d7
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-07 16:21:00 +0100

    Layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,17 @@
 // import Title from "../shared/Title/Title";
-import SideBar from "../../components/SideBar/SideBar";
-import WorkPlace from "../../components/WorkPlace/WorkPlace";
-import css from "./HomePage.module.css";
+import SideBar from '../../components/SideBar/SideBar';
+import WorkPlace from '../../components/WorkPlace/WorkPlace';
+import css from './HomePage.module.css';
+import Layout from '../../shared/components/Layout/Layout';
+import AppBar from '../../components/AppBar/AppBar';
 
 export default function HomePage() {
-  return (
-    <div className={css.container}>
-      <SideBar />
-      <WorkPlace />
-    </div>
-  );
+	return (
+		<div className={css.container}>
+			<Layout>
+				<SideBar />
+			</Layout>
+			<WorkPlace />
+		</div>
+	);
 }
