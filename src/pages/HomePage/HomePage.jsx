48e4bc5837108fcf9344b54c5db082a4dc28be09
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-07 16:21:26 +0100

    AppBar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,7 +3,6 @@ import SideBar from '../../components/SideBar/SideBar';
 import WorkPlace from '../../components/WorkPlace/WorkPlace';
 import css from './HomePage.module.css';
 import Layout from '../../shared/components/Layout/Layout';
-import AppBar from '../../components/AppBar/AppBar';
 
 export default function HomePage() {
 	return (
