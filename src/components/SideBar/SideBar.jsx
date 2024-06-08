5d6926d438af076d2ec5ff877fb3395437baed7b
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-06-09 00:07:20 +0300

    Made stiles + burger function

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,7 +6,7 @@ import css from "./SideBar.module.css";
 import NeedHelpModal from "../NeedHelpModal/NeedHelpModal";
 import clsx from "clsx";
 
-export default function SideBar() {
+export default function SideBar({ isSidebarOpen }) {
   const [isOpenCreateModal, setIsOpenCreateModal] = useState(false);
   const [isOpenHelpModal, setIsOpenHelpModal] = useState(false);
 
@@ -21,56 +21,68 @@ export default function SideBar() {
     <>
       {isOpenCreateModal && alert("add task")}
       {isOpenHelpModal && <NeedHelpModal handleHelpModal={handleHelpModal} />}
-      <aside className={clsx(css.container, css.isClosed)}>
-        <LogoComponent />
-        <h4 className={css.title}>My boards</h4>
-        <div className={css.wrapper}>
-          <p className={css.text}>Create a new board</p>
-          <button
-            type="button"
-            onClick={handleCreateModal}
-            className={css.addBtn}
-          >
-            <svg className={css.btnIcon} width="20" height="20">
-              <use href="../../../public/sprite.svg#icon-plus"></use>
-            </svg>
-          </button>
-        </div>
-        <BoardNavigation />
-        <div className={css.helpWrapper}>
-          <img className={css.img} src={img} alt="Plant" />
-          <p>
-            If you need help with <span className={css.span}>TaskPro</span>,
-            check out our support resources or reach out to our customer support
-            team.
-          </p>
-          <button
-            type="button"
-            onClick={handleHelpModal}
-            className={css.helpBtn}
-          >
-            <svg className={css.helpIcon} width="20" height="20">
-              <use href="../../../public/sprite.svg#icon-container"></use>
-            </svg>
-            Need help?
-          </button>
-        </div>
-        <button
-          className={css.logoutBtn}
-          type="button"
-          onClick={() => {
-            alert("log out");
-          }}
+      <div className={css.backdrop}>
+        <aside
+          className={clsx(
+            css.container,
+            {
+              [css.isClosed]: !isSidebarOpen,
+            },
+            {
+              [css.backdrop]: isSidebarOpen,
+            }
+          )}
         >
-          <svg className={css.logoutIcon} width="32" height="32">
-            <use
-              className={css.logoutIcon}
-              href="../../../public/sprite.svg#icon-login"
-            ></use>
-          </svg>
-          Log out
-        </button>
-      </aside>
+          <LogoComponent />
+          <h4 className={css.title}>My boards</h4>
+          <div className={css.wrapper}>
+            <p className={css.text}>Create a new board</p>
+            <button
+              type="button"
+              onClick={handleCreateModal}
+              className={css.addBtn}
+            >
+              <svg className={css.btnIcon} width="20" height="20">
+                <use href="../../../public/sprite.svg#icon-plus"></use>
+              </svg>
+            </button>
+          </div>
+          <BoardNavigation />
+          <div className={css.helpWrapper}>
+            <img className={css.img} src={img} alt="Plant" />
+            <p>
+              If you need help with <span className={css.span}>TaskPro</span>,
+              check out our support resources or reach out to our customer
+              support team.
+            </p>
+            <button
+              type="button"
+              onClick={handleHelpModal}
+              className={css.helpBtn}
+            >
+              <svg className={css.helpIcon} width="20" height="20">
+                <use href="../../../public/sprite.svg#icon-container"></use>
+              </svg>
+              Need help?
+            </button>
+          </div>
+          <button
+            className={css.logoutBtn}
+            type="button"
+            onClick={() => {
+              alert("log out");
+            }}
+          >
+            <svg className={css.logoutIcon} width="32" height="32">
+              <use
+                className={css.logoutIcon}
+                href="../../../public/sprite.svg#icon-login"
+              ></use>
+            </svg>
+            Log out
+          </button>
+        </aside>
+      </div>
     </>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,20 @@
-import SideBar from '../components/SideBar/SideBar';
+import SideBar from "../components/SideBar/SideBar";
+import { useState } from "react";
 
-import Layout from '../shared/components/Layout/Layout';
-import MainContent from '../components/MainContent/MainContent';
+import Layout from "../shared/components/Layout/Layout";
+import MainContent from "../components/MainContent/MainContent";
 
 export default function HomePage() {
-	return (
-		<Layout>
-			<SideBar />
-			<MainContent />
-		</Layout>
-	);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prevState) => !prevState);
+  };
+
+  return (
+    <Layout>
+      <SideBar isSidebarOpen={isSidebarOpen} />
+      <MainContent toggleSidebar={toggleSidebar} />
+    </Layout>
+  );
 }
