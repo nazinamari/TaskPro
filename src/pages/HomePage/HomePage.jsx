d93e0c99b84bf716097a4acab513c1f9b174b31e
Bohdana1958 <139041084+Bohdana1958@users.noreply.github.com>
2024-06-08 11:50:46 +0200

    Update header

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,12 @@
 // import Title from "../shared/Title/Title";
 import SideBar from "../../components/SideBar/SideBar";
-import Header from "../../components/Header/Header";
+// import Header from "../../components/Header/Header";
 // import WorkPlace from "../components/WorkPlace/WorkPlace";
 
 export default function HomePage() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <SideBar />
       {/* <WorkPlace /> */}
     </div>
