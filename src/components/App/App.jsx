f1d817f5c2a26e7144257ccff95f33bf380a3b47
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-08 22:37:04 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,18 @@
 import { Suspense } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import WelcomePage from "../../pages/WelcomePage/WelcomePage";
-import AuthPage from "../../pages/AuthPage/AuthPage";
-
+import AuthPage from "../../pages/AuthPage";
 import RegisterForm from "../RegisterForm/RegisterForm";
 import LoginForm from "../LoginForm/LoginForm";
-import HomePage from "../../pages/HomePage/HomePage";
-import BoardPage from "../../pages/BoardPage/BoardPage";
+import HomePage from "../../pages/HomePage";
+import BoardPage from "../../pages/BoardPage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-// import Container from "../../shared/components/Container/Container";
 
 export default function App() {
   return (
     <div>
       <Suspense fallback={null}>
         <Routes>
-          <Route path="/" element={<Navigate to="/welcome" />} />
           <Route path="/welcome" element={<WelcomePage />} />
           <Route path="/auth" element={<AuthPage />}>
             <Route path="register" element={<RegisterForm />} />
@@ -28,7 +25,4 @@ export default function App() {
       </Suspense>
     </div>
   );
-
-		</div>
-	);
 }
