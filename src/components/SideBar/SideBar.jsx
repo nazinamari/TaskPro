79beca61ab450a470be7e84482488a012eee518b
Dikens27 <kar.karovich321@gmail.com>
2024-06-09 20:01:40 +0300

    some commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,88 +1,80 @@
 import { useState, useCallback } from "react";
 import BoardNavigation from "../BoardNavigation/BoardNavigation";
-import LogoComponent from "../../shared/components/LogoComponent/LogoComponent";
-import img from "../../../public/2.png";
+import LogoComponent from "../LogoComponent/LogoComponent";
 import css from "./SideBar.module.css";
 import NeedHelpModal from "../NeedHelpModal/NeedHelpModal";
-import clsx from "clsx";
 
-export default function SideBar({ isSidebarOpen }) {
+export default function SideBar() {
   const [isOpenCreateModal, setIsOpenCreateModal] = useState(false);
   const [isOpenHelpModal, setIsOpenHelpModal] = useState(false);
 
   const handleCreateModal = useCallback(() => {
     setIsOpenCreateModal((prevState) => !prevState);
   }, []);
+
   const handleHelpModal = useCallback(() => {
     setIsOpenHelpModal((prevState) => !prevState);
   }, []);
 
   return (
     <>
-      {isOpenCreateModal && alert("add task")}
-      {isOpenHelpModal && <NeedHelpModal handleHelpModal={handleHelpModal} />}
-      <div className={css.backdrop}>
-        <aside
-          className={clsx(
-            css.container,
-            {
-              [css.isClosed]: !isSidebarOpen,
-            },
-            {
-              [css.backdrop]: isSidebarOpen,
-            }
-          )}
-        >
-          <LogoComponent />
-          <h4 className={css.title}>My boards</h4>
-          <div className={css.wrapper}>
-            <p className={css.text}>Create a new board</p>
-            <button
-              type="button"
-              onClick={handleCreateModal}
-              className={css.addBtn}
-            >
-              <svg className={css.btnIcon} width="20" height="20">
-                <use href="../../../public/sprite.svg#icon-plus"></use>
-              </svg>
-            </button>
-          </div>
-          <BoardNavigation />
-          <div className={css.helpWrapper}>
-            <img className={css.img} src={img} alt="Plant" />
-            <p>
-              If you need help with <span className={css.span}>TaskPro</span>,
-              check out our support resources or reach out to our customer
-              support team.
-            </p>
-            <button
-              type="button"
-              onClick={handleHelpModal}
-              className={css.helpBtn}
-            >
-              <svg className={css.helpIcon} width="20" height="20">
-                <use href="../../../public/sprite.svg#icon-container"></use>
-              </svg>
-              Need help?
-            </button>
-          </div>
+      {isOpenCreateModal && <p>MODAL WINDOW</p>}
+      {/* Додати компонент нової таски */}
+
+      {isOpenHelpModal && <NeedHelpModal />}
+      <aside className={css.container}>
+        <LogoComponent />
+        <h4 className={css.title}>My boards</h4>
+        <div className={css.wrapper}>
+          <p className={css.text}>Create a new board</p>
           <button
-            className={css.logoutBtn}
             type="button"
-            onClick={() => {
-              alert("log out");
-            }}
+            onClick={handleCreateModal}
+            className={css.addBtn}
           >
-            <svg className={css.logoutIcon} width="32" height="32">
-              <use
-                className={css.logoutIcon}
-                href="../../../public/sprite.svg#icon-login"
-              ></use>
+            <svg className={css.btnIcon}>
+              <use href="../../../public/sprite.svg#icon-plus"></use>
             </svg>
-            Log out
           </button>
-        </aside>
-      </div>
+        </div>
+        <BoardNavigation />
+        <div className={css.helpWrapper}>
+          <div className={css.image}></div> {/* Додати image */}
+          <p>
+            If you need help with <span className={css.span}>TaskPro</span>,
+            check out our support resources or reach out to our customer support
+            team.
+          </p>
+          <button
+            type="button"
+            onClick={handleHelpModal}
+            className={css.helpBtn}
+          >
+            <svg className={css.helpIcon}>
+              <use href="../../../public/sprite.svg#icon-pencil-01"></use>
+            </svg>
+            Need help? {/* Додати icon */}
+          </button>
+        </div>
+        <button
+          className={css.logoutBtn}
+          type="button"
+          onClick={() => {
+            // dispatch(logOut())
+            //   .unwrap()
+            //   .then()
+            //   .catch(() => {
+            //     errToast();
+            //   });
+            alert("log out");
+          }}
+        >
+          <svg className={css.helpIcon}>
+            <use href="../../../public/sprite.svg#icon-login"></use>
+          </svg>
+          Log out
+        </button>
+      </aside>
     </>
   );
 }
