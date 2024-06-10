9f3c7620145c7ba9576dab66fab5b6806dee48d9
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-06-10 23:22:17 +0300

    made  board page

--- src/components/WorkPlace/WorkPlace.jsx
+++ src/components/WorkPlace/WorkPlace.jsx
@@ -1,5 +1,46 @@
 import css from "./WorkPlace.module.css";
+import TaskCard from "../CardTask/CardTask";
+
+const arr = [
+  { name: "to do" },
+  { name: "in progres" },
+  { name: "revue" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+  { name: "done" },
+];
 
 export default function WorkPlace() {
-  return <div className={css.container}></div>;
+  return (
+    <div className={css.container}>
+      <div>
+        <ul className={css.list}>
+          {arr.map(({ name }) => (
+            <li key={(~~(Math.random() * 1e8)).toString(16)}>
+              <p>{name}</p>
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+              <TaskCard />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className={css.addColumnBtn}>Add column</button>
+    </div>
+  );
 }
