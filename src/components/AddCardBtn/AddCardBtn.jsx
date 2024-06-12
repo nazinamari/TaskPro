3c813eb581cf316523f98a8f151066349f50e106
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-06-12 18:47:53 +0300

    add modal

--- src/components/AddCardBtn/AddCardBtn.jsx
+++ src/components/AddCardBtn/AddCardBtn.jsx
@@ -1,44 +1,14 @@
-// import Icon from "../../shared/components/Icon/Icon";
-// import styles from "./AddCardBtn.module.css";
-
-// const AddCardBtn = ({ onClick }) => {
-//   return (
-//     <button className={styles.addButton} onClick={onClick}>
-//       <div className={styles.iconContainer}>
-//         <Icon id="icon-plus" width="16" height="16" className={styles.icon} />
-//       </div>
-//       Add another card
-//     </button>
-//   );
-// };
-
-// export default AddCardBtn;
-import { useState } from "react";
-import AddCardForm from "../AddCardForm/AddCardForm";
 import Icon from "../../shared/components/Icon/Icon";
 import styles from "./AddCardBtn.module.css";
 
-const AddCardBtn = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleClick = () => {
-    setShowModal(true);
-  };
-
-  const handleClose = () => {
-    setShowModal(false);
-  };
-
+const AddCardBtn = ({ onClick }) => {
   return (
-    <>
-      <button className={styles.addButton} onClick={handleClick}>
-        <div className={styles.iconContainer}>
-          <Icon id="icon-plus" width="16" height="16" className={styles.icon} />
-        </div>
-        Add another card
-      </button>
-      {showModal && <AddCardForm onClose={handleClose} />}
-    </>
+    <button className={styles.addButton} onClick={onClick}>
+      <div className={styles.iconContainer}>
+        <Icon id="icon-plus" width="16" height="16" className={styles.icon} />
+      </div>
+      Add another card
+    </button>
   );
 };
 
