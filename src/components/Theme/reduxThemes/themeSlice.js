e4254346fa3cb17597968425a94117b08ea53c02
IvanBuzin <ua.buzin@gmail.com>
2024-06-13 01:52:13 +0300

    reduxThemes

--- src/components/Theme/reduxThemes/themeSlice.js
+++ src/components/Theme/reduxThemes/themeSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit/toolkit";
 import data from "../data/theme.json";
 import { fetchTheme, updateTheme } from "./operations";
 
