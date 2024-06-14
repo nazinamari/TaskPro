6a48969238002740e9ae09cc5af51740c1625475
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-14 12:42:07 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "../src/components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { persistor, store } from "../redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "../redux/store.js";
 import { Provider } from "react-redux";
-import { store } from "../redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
