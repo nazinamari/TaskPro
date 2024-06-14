e75215564626557eeb3be6f9eeb0d70f29fa14a5
Yarina Oleksii <aleksey.yarina@gmail.com>
2024-06-14 18:33:53 +0300

    fix: buttons edit and delete

--- src/components/CardTask/CardTask.jsx
+++ src/components/CardTask/CardTask.jsx
@@ -1,65 +1,3 @@
-// import Icon from "../../shared/components/Icon/Icon";
-// import styles from "./CardTask.module.css";
-// import "../../shared/styles/variables.css";
-
-// const TaskCard = ({ title, description, labelColor, deadline }) => {
-//   return (
-//     <div className={`${styles.card} ${styles[labelColor]}`}>
-//       <p className={styles.header}>{title}</p>
-//       <p className={styles.description}>{description}</p>
-//       <div className={styles.separator}></div>
-//       <div className={styles.footer}>
-//         <div className={styles.containerPriorityDeadline}>
-//           <div className={styles.priority}>
-//             <span className={styles.priorityLabel}>Priority</span>
-//             <div className={styles.priorityContainer}>
-//               <span
-//                 className={`${styles.priorityColor} ${styles[labelColor]}`}
-//               ></span>
-//               <span className={styles.priorityText}>{labelColor}</span>
-//             </div>
-//           </div>
-//           <div className={styles.deadline}>
-//             <span className={styles.deadlineLabel}>Deadline</span>
-//             <span className={styles.deadlineValue}>
-//               {new Date(deadline)
-//                 .toLocaleDateString("en-GB")
-//                 .replace(/\./g, "/")}
-//             </span>
-//           </div>
-//         </div>
-//         <div className={styles.actions}>
-//           <button className={styles.actionButton}>
-//             <Icon
-//               id="icon-arrow-circler"
-//               width="16"
-//               height="16"
-//               className={styles.icon}
-//             />
-//           </button>
-//           <button className={styles.actionButton}>
-//             <Icon
-//               id="icon-pencil"
-//               width="16"
-//               height="16"
-//               className={styles.icon}
-//             />
-//           </button>
-//           <button className={styles.actionButton}>
-//             <Icon
-//               id="icon-trash"
-//               width="16"
-//               height="16"
-//               className={styles.icon}
-//             />
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default TaskCard;
 import Icon from "../../shared/components/Icon/Icon";
 import styles from "./CardTask.module.css";
 import "../../shared/styles/variables.css";
@@ -71,6 +9,7 @@ const TaskCard = ({
   labelColor,
   deadline,
   onRemove,
+  onEdit,
 }) => {
   return (
     <div className={`${styles.card} ${styles[labelColor]}`}>
@@ -106,7 +45,12 @@ const TaskCard = ({
               className={styles.icon}
             />
           </button>
-          <button className={styles.actionButton}>
+          <button
+            className={styles.actionButton}
+            onClick={() =>
+              onEdit({ id, title, description, labelColor, deadline })
+            }
+          >
             <Icon
               id="icon-pencil"
               width="16"
--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,68 +1,18 @@
-// import { useState } from "react";
-// import Modal from "react-modal";
-// import ToDo from "../ToDo/ToDo";
-// import TaskCard from "../CardTask/CardTask";
-// import AddCardBtn from "../AddCardBtn/AddCardBtn";
-// import AddCardModal from "../AddCardModal/AddCardModal";
-// import styles from "./Column.module.css";
-
-// const Column = ({ title: initialTitle, onDelete }) => {
-//   const [cards, setCards] = useState([]);
-//   const [title, setTitle] = useState(initialTitle);
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const handleAddCard = (newCard) => {
-//     setCards([...cards, { ...newCard, id: cards.length }]);
-//     setIsModalOpen(false);
-//   };
-
-//   const handleEditTitle = (newTitle) => {
-//     setTitle(newTitle);
-//   };
-
-//   const openModal = () => {
-//     setIsModalOpen(true);
-//   };
-
-//   const closeModal = () => {
-//     setIsModalOpen(false);
-//   };
-
-//   return (
-//     <div className={styles.column}>
-//       <ToDo title={title} onEditTitle={handleEditTitle} onDelete={onDelete} />
-//       <div className={styles.cards}>
-//         {cards.map((card) => (
-//           <TaskCard key={card.id} {...card} />
-//         ))}
-//       </div>
-//       <AddCardBtn onClick={openModal} />
-//       <Modal
-//         isOpen={isModalOpen}
-//         onRequestClose={closeModal}
-//         contentLabel="Add Card Modal"
-//         className={styles.modal}
-//         overlayClassName={styles.overlay}
-//       >
-//         <AddCardModal onAddCard={handleAddCard} onClose={closeModal} />
-//       </Modal>
-//     </div>
-//   );
-// };
-
-// export default Column;
 import { useState } from "react";
 import Modal from "react-modal";
 import ToDo from "../ToDo/ToDo";
 import TaskCard from "../CardTask/CardTask";
 import AddCardBtn from "../AddCardBtn/AddCardBtn";
 import AddCardModal from "../AddCardModal/AddCardModal";
+import EditCardModal from "../EditCardModal/EditCardModal";
 import styles from "./Column.module.css";
 
 const Column = ({ title: initialTitle, onDelete }) => {
   const [cards, setCards] = useState([]);
   const [title, setTitle] = useState(initialTitle);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [currentCard, setCurrentCard] = useState(null);
 
   const handleAddCard = (newCard) => {
     setCards([...cards, { ...newCard, id: cards.length }]);
@@ -71,6 +21,13 @@ const Column = ({ title: initialTitle, onDelete }) => {
 
   const handleRemoveCard = (id) => {
     setCards(cards.filter((card) => card.id !== id));
+  };
+
+  const handleEditCard = (updatedCard) => {
+    setCards(
+      cards.map((card) => (card.id === updatedCard.id ? updatedCard : card))
+    );
+    setIsEditModalOpen(false);
   };
 
   const handleEditTitle = (newTitle) => {
@@ -85,12 +42,27 @@ const Column = ({ title: initialTitle, onDelete }) => {
     setIsModalOpen(false);
   };
 
+  const openEditModal = (card) => {
+    setCurrentCard(card);
+    setIsEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setIsEditModalOpen(false);
+    setCurrentCard(null);
+  };
+
   return (
     <div className={styles.column}>
       <ToDo title={title} onEditTitle={handleEditTitle} onDelete={onDelete} />
       <div className={styles.cards}>
         {cards.map((card) => (
-          <TaskCard key={card.id} {...card} onRemove={handleRemoveCard} />
+          <TaskCard
+            key={card.id}
+            {...card}
+            onRemove={handleRemoveCard}
+            onEdit={openEditModal}
+          />
         ))}
       </div>
       <AddCardBtn onClick={openModal} />
@@ -103,6 +75,21 @@ const Column = ({ title: initialTitle, onDelete }) => {
       >
         <AddCardModal onAddCard={handleAddCard} onClose={closeModal} />
       </Modal>
+      {currentCard && (
+        <Modal
+          isOpen={isEditModalOpen}
+          onRequestClose={closeEditModal}
+          contentLabel="Edit Card Modal"
+          className={styles.modal}
+          overlayClassName={styles.overlay}
+        >
+          <EditCardModal
+            card={currentCard}
+            onEditCard={handleEditCard}
+            onClose={closeEditModal}
+          />
+        </Modal>
+      )}
     </div>
   );
 };
