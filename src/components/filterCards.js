b9b81d1bc3f1aa8c0cc405647092bc376dba7ea6
IvanBuzin <ua.buzin@gmail.com>
2024-06-15 13:10:23 +0300

    Create filterCards.js

---
+++ src/components/filterCards.js
@@ -0,0 +1,11 @@
+export const filterCards = (cards, filter) => {
+  let filteredCards = [...cards];
+  if (cards.length > 1) {
+    filteredCards.sort((a, b) => a.index - b.index);
+  }
+  if (filter === "all") return filteredCards;
+
+  filteredCards = cards.filter((card) => card.priority === filter);
+
+  return filteredCards;
+};
