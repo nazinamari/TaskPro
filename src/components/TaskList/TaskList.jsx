4d084240f1a854af7476ea136948cc0d4e5c0995
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-15 22:37:45 +0100

    selector

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,71 +1,56 @@
-// import css from "./TaskList.module.css";
-// import TaskCard from "../CardTask/CardTask";
-
-// const arr = [];
-// export default function TaskList() {
-//   return (
-//     <ul className={css.list}>
-//       {arr.map(({ name }) => (
-//         <li key={(~~(Math.random() * 1e8)).toString(16)}>
-//           <TaskCard name={name} />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
-  fetchAllCards,
-  selectAllCards,
-  selectLoading,
-  selectError,
-} from "../../../redux/cards/slice";
-import TaskCard from "../CardTask/CardTask";
-import styles from "./TaskList.module.css";
+	fetchAllCards,
+	selectAllCards,
+	selectLoading,
+	selectError,
+} from '../../../redux/cards/selectors';
+import TaskCard from '../CardTask/CardTask';
+import styles from './TaskList.module.css';
 
 const TaskList = ({ onEdit, onRemove }) => {
-  const dispatch = useDispatch();
-  const cards = useSelector(selectAllCards);
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+	const dispatch = useDispatch();
+	const cards = useSelector(selectAllCards);
+	const loading = useSelector(selectLoading);
+	const error = useSelector(selectError);
 
-  useEffect(() => {
-    dispatch(fetchAllCards());
-  }, [dispatch]);
+	useEffect(() => {
+		dispatch(fetchAllCards());
+	}, [dispatch]);
 
-  const handleRemove = (id) => {
-    onRemove(id);
-  };
+	const handleRemove = (id) => {
+		onRemove(id);
+	};
 
-  const handleEdit = (card) => {
-    onEdit(card);
-  };
+	const handleEdit = (card) => {
+		onEdit(card);
+	};
 
-  const handleMove = () => {
-    // Реализовать функцию перемещения карточки   const handleMove = (id) => {
-  };
+	const handleMove = () => {
+		// Реализовать функцию перемещения карточки   const handleMove = (id) => {
+	};
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+	if (loading) return <div>Loading...</div>;
+	if (error) return <div>Error: {error}</div>;
 
-  return (
-    <div className={styles.taskList}>
-      {cards.map((card) => (
-        <TaskCard
-          key={card.id}
-          id={card.id}
-          title={card.title}
-          description={card.description}
-          labelColor={card.labelColor}
-          deadline={card.deadline}
-          onRemove={handleRemove}
-          onEdit={handleEdit}
-          onMove={handleMove}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className={styles.taskList}>
+			{cards.map((card) => (
+				<TaskCard
+					key={card.id}
+					id={card.id}
+					title={card.title}
+					description={card.description}
+					labelColor={card.labelColor}
+					deadline={card.deadline}
+					onRemove={handleRemove}
+					onEdit={handleEdit}
+					onMove={handleMove}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default TaskList;
