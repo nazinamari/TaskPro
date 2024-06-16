8d8c396ea29215b849686f7dbe6aac02e3d82d05
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-16 23:11:36 +0100

    favicon

--- src/components/ThemeT/ThemaSwitcher/Theme.jsx
+++ src/components/ThemeT/ThemaSwitcher/Theme.jsx
@@ -2,8 +2,8 @@ import Button from '../../../shared/components/Button/Button';
 
 export default function Theme({ data, className }) {
 	return (
-		<div>
+		<Button>
 			<div className={className}>{data}</div>
-		</div>
+		</Button>
 	);
 }
