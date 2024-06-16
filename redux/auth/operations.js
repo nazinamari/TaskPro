51a917c3bdd2bd2a6760e54ba3adfac763afed0c
IvanBuzin <ua.buzin@gmail.com>
2024-06-16 14:50:46 +0300

    delete Them

--- redux/auth/operations.js
+++ redux/auth/operations.js
@@ -77,24 +77,6 @@ export const refreshUser = createAsyncThunk(
 //   return thunkApi.rejectWithValue(error.message);
 // }
 
-export const changeTheme = createAsyncThunk(
-  "auth/theme",
-  async (credentials, thunkAPI) => {
-    const state = thunkAPI.getState();
-    const persistedToken = state.auth.token;
-
-    if (persistedToken === null) {
-      return thunkAPI.rejectWithValue("Unable to fetch user");
-    }
-    try {
-      setAuthHeader(persistedToken);
-      const response = await axios.patch("api/users/changeTheme", credentials);
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
 //Need help
 export const sendHelpRequest = createAsyncThunk(
   "user/help",
--- src/components/Theme/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { themeSlice } from "./themeSlice";
-
-export const store = configureStore({
-  reducer: {
-    theme: themeSlice.reducer,
-  },
-});
