ac753626bdabc0fe2d522b3bba7796f1221718e7
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-18 21:56:21 +0100

    pritier

--- src/components/EditBoardModal/EditBoardModal.jsx
+++ src/components/EditBoardModal/EditBoardModal.jsx
@@ -12,152 +12,162 @@ import picData from './data/pic.json';
 import Picture from '../../shared/components/Pic/Pic';
 
 export default function EditBoardModal({ onClose, title }) {
-	const board = useSelector(selectBoard);
-	console.log(board);
-	const [selectedIcon, setSelectedIcon] = useState(
-		board.board.icon || icons[0].value
-	);
-	const [selectedBg, setSelectedBg] = useState(
-		board.board.background || 'bg-1'
-	);
-	const [boardTitle, setBoardTitle] = useState(title);
+    const board = useSelector(selectBoard);
+    console.log(board);
+    const [selectedIcon, setSelectedIcon] = useState(
+        board.board.icon || icons[0].value,
+    );
+    const [selectedBg, setSelectedBg] = useState(
+        board.board.background || 'bg-1',
+    );
+    const [boardTitle, setBoardTitle] = useState(title);
 
-	useEffect(() => {
-		setSelectedIcon(board.board.icon || icons[0].value);
-		setSelectedBg(board.board.background || 'bg-1');
-		setBoardTitle(board.board.title);
-	}, [board]);
+    useEffect(() => {
+        setSelectedIcon(board.board.icon || icons[0].value);
+        setSelectedBg(board.board.background || 'bg-1');
+        setBoardTitle(board.board.title);
+    }, [board]);
 
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const onSubmit = (event) => {
-		event.preventDefault();
-		const id = board.board._id;
-		const data = {
-			title: boardTitle,
-			icon: selectedIcon,
-			background: selectedBg,
-		};
-		dispatch(editBoard({ boardId: id, data }))
-			.unwrap()
-			.then(() => {
-				console.log('update successfully'); // додати тост
-			})
-			.catch((error) => {
-				console.error(error.message);
-			});
-		onClose();
-		dispatch(getBoardById(id));
-	};
+    const onSubmit = event => {
+        event.preventDefault();
+        const id = board.board._id;
+        const data = {
+            title: boardTitle,
+            icon: selectedIcon,
+            background: selectedBg,
+        };
+        dispatch(editBoard({ boardId: id, data }))
+            .unwrap()
+            .then(() => {
+                console.log('update successfully'); // додати тост
+            })
+            .catch(error => {
+                console.error(error.message);
+            });
+        onClose();
+        dispatch(getBoardById(id));
+    };
 
-	const stopPropagation = (event) => {
-		event.stopPropagation();
-	};
+    const stopPropagation = event => {
+        event.stopPropagation();
+    };
 
-	useEffect(() => {
-		const handleKeyDown = (event) => {
-			if (event.key === 'Escape') {
-				onClose();
-			}
-		};
+    useEffect(() => {
+        const handleKeyDown = event => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+        };
 
-		document.addEventListener('keydown', handleKeyDown);
-		return () => {
-			document.removeEventListener('keydown', handleKeyDown);
-		};
-	}, [onClose]);
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onClose]);
 
-	const data = picData.map((item) => {
-		return {
-			...item,
-			url: images[item.url],
-			url2x: images[item.url2x],
-		};
-	});
+    const data = picData.map(item => {
+        return {
+            ...item,
+            url: images[item.url],
+            url2x: images[item.url2x],
+        };
+    });
 
-	console.log(...data);
+    console.log(...data);
 
-	return (
-		<div className={css.container} onClick={onClose}>
-			<div className={css.modalWindow} onClick={stopPropagation}>
-				<div className={css.modalWindowContent}>
-					<button className={css.closeBtn} onClick={onClose}>
-						<Icon
-							id="icon-close"
-							width="18"
-							height="24"
-							className={css.iconClose}
-							alt="icon-close"
-						/>
-					</button>
-					<h2 className={css.title}>Edit board</h2>
+    return (
+        <div className={css.container} onClick={onClose}>
+            <div className={css.modalWindow} onClick={stopPropagation}>
+                <div className={css.modalWindowContent}>
+                    <button className={css.closeBtn} onClick={onClose}>
+                        <Icon
+                            id="icon-close"
+                            width="18"
+                            height="24"
+                            className={css.iconClose}
+                            alt="icon-close"
+                        />
+                    </button>
+                    <h2 className={css.title}>Edit board</h2>
 
-					<form className={css.form} onSubmit={onSubmit}>
-						<input
-							className={css.input}
-							type="text"
-							value={boardTitle}
-							onChange={(e) => setBoardTitle(e.target.value)}
-						/>{' '}
-						<div className={css.formContainer}>
-							<h3 className={css.iconsTitle}>Icons</h3>
-							<ul className={css.iconsContainer}>
-								{icons.map((icon) => (
-									<li key={icon.value} className={css.iconLabel}>
-										<label htmlFor={icon.id} className={css.iconLabel}>
-											<input
-												type="radio"
-												value={icon.value}
-												id={icon.id}
-												className={css.iconRadio}
-												onChange={() => setSelectedIcon(icon.value)}
-												checked={selectedIcon === icon.value}
-											/>
+                    <form className={css.form} onSubmit={onSubmit}>
+                        <input
+                            className={css.input}
+                            type="text"
+                            value={boardTitle}
+                            onChange={e => setBoardTitle(e.target.value)}
+                        />{' '}
+                        <div className={css.formContainer}>
+                            <h3 className={css.iconsTitle}>Icons</h3>
+                            <ul className={css.iconsContainer}>
+                                {icons.map(icon => (
+                                    <li
+                                        key={icon.value}
+                                        className={css.iconLabel}
+                                    >
+                                        <label
+                                            htmlFor={icon.id}
+                                            className={css.iconLabel}
+                                        >
+                                            <input
+                                                type="radio"
+                                                value={icon.value}
+                                                id={icon.id}
+                                                className={css.iconRadio}
+                                                onChange={() =>
+                                                    setSelectedIcon(icon.value)
+                                                }
+                                                checked={
+                                                    selectedIcon === icon.value
+                                                }
+                                            />
 
-											<Icon
-												id={icon.id}
-												alt={icon.alt}
-												width={icon.width}
-												height={icon.height}
-												className={css.iconImage}
-											/>
-										</label>
-									</li>
-								))}
-							</ul>
-						</div>
-						<h3 className={css.iconsTitle}>Background</h3>
-						<ul className={css.bgList}>
-							<div className={css.imgWrap}>
-								{data.map((item, index) => (
-									<Picture
-										key={index}
-										url={item.url}
-										url2x={item.url2x}
-										height="100%"
-										width="auto"
-									/>
-								))}
-							</div>
-						</ul>
-						<button type="submit" className={css.editBtn}>
-							<div className={css.wrapper}>
-								<Icon
-									id="icon-plus"
-									width="14"
-									height="14"
-									className={css.iconPlus}
-									alt="icon-close"
-								/>
-							</div>
+                                            <Icon
+                                                id={icon.id}
+                                                alt={icon.alt}
+                                                width={icon.width}
+                                                height={icon.height}
+                                                className={css.iconImage}
+                                            />
+                                        </label>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <h3 className={css.iconsTitle}>Background</h3>
+                        <ul className={css.bgList}>
+                            <div className={css.imgWrap}>
+                                {data.map((item, index) => (
+                                    <Picture
+                                        key={index}
+                                        url={item.url}
+                                        url2x={item.url2x}
+                                        height="100%"
+                                        width="auto"
+                                    />
+                                ))}
+                            </div>
+                        </ul>
+                        <button type="submit" className={css.editBtn}>
+                            <div className={css.wrapper}>
+                                <Icon
+                                    id="icon-plus"
+                                    width="14"
+                                    height="14"
+                                    className={css.iconPlus}
+                                    alt="icon-close"
+                                />
+                            </div>
 
-							<p className={css.createText}>Edit</p>
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	);
+                            <p className={css.createText}>Edit</p>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 // {
