e39b0d745f748f119b7189a9854faa4ae0c128fd
IvanBuzin <ua.buzin@gmail.com>
2024-06-19 12:02:21 +0300

    ProjectModal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,24 @@
-import { Suspense, lazy, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { refreshToken } from "../../redux/auth/operations.js";
-import { RestrictedRoute } from "../Routes/RestrictedRoute";
-import { Toaster } from "react-hot-toast";
-import { PrivateRoute } from "../Routes/PrivateRoute";
-import { refreshUser } from "../../redux/user/operations.js";
+import { Suspense, lazy, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { refreshToken } from '../../redux/auth/operations.js';
+import { RestrictedRoute } from '../Routes/RestrictedRoute';
+import { Toaster } from 'react-hot-toast';
+import { PrivateRoute } from '../Routes/PrivateRoute';
+import { refreshUser } from '../../redux/user/operations.js';
+import ProjectModal from '../ProjectModal/ProjectModal.jsx';
+import { useState } from 'react';
 
-const WelcomePage = lazy(() => import("../../pages/WelcomePage/WelcomePage"));
-const AuthPage = lazy(() => import("../../pages/AuthPage"));
-const RegisterForm = lazy(() => import("../RegisterForm/RegisterForm"));
-const LoginForm = lazy(() => import("../LoginForm/LoginForm"));
-const HomePage = lazy(() => import("../../pages/HomePage"));
-const BoardPage = lazy(() => import("../../pages/BoardPage"));
+const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
+const AuthPage = lazy(() => import('../../pages/AuthPage'));
+const RegisterForm = lazy(() => import('../RegisterForm/RegisterForm'));
+const LoginForm = lazy(() => import('../LoginForm/LoginForm'));
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const BoardPage = lazy(() => import('../../pages/BoardPage'));
 const NotFoundPage = lazy(() =>
-  import("../../pages/NotFoundPage/NotFoundPage")
+  import('../../pages/NotFoundPage/NotFoundPage'),
 );
+const [modalActive, setModalActive] = useState(true);
 
 export default function App() {
   const dispatch = useDispatch();
@@ -70,6 +73,27 @@ export default function App() {
         </Routes>
       </Suspense>
       <Toaster position="top-center" />
+      <div>
+        <button className="open-btn" onClick={() => setModalActive(true)}>
+          Команда проекту
+        </button>
+        <p>проект</p>
+      </div>
+      <ProjectModal>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab culpa,
+          voluptatem repudiandae vitae molestiae, eveniet totam iusto, nisi
+          assumenda nobis hic officiis aliquid laudantium expedita? Aliquam
+          quisquam facilis rem doloremque!
+        </p>
+      </ProjectModal>
+      <ProjectModal active={modalActive} setActive={setModalActive}>
+        <form action="">
+          <input type="text" />
+          <input type="text" />
+          <button></button>
+        </form>
+      </ProjectModal>
     </div>
   );
 }
