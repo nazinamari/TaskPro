36af70f660bf6d7315c808ef14199daea8a65968
Yevhenii Pozniak <jekamanu@gmail.com>
2024-06-20 16:20:13 +0300

    add teamPhotos

---
+++ src/components/TeamPhotos/PhotoGallery.jsx
@@ -0,0 +1,29 @@
+import styles from '';
+
+export const PhotoGallery = () => {
+  const photos = [
+    { id: 1, src: 'photo1.jpg', alt: 'Photo 1' },
+    { id: 2, src: 'photo2.jpg', alt: 'Photo 2' },
+    { id: 3, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 4, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 5, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 6, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 7, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 8, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 9, src: 'photo3.jpg', alt: 'Photo 3' },
+    { id: 10, src: 'photo3.jpg', alt: 'Photo 3' },
+  ];
+
+  return (
+    <div className={styles.gallery}>
+      {photos.map(photo => (
+        <img
+          key={photo.id}
+          src={photo.src}
+          alt={photo.alt}
+          className={styles.photo}
+        />
+      ))}
+    </div>
+  );
+};
---
+++ src/components/TeamPhotos/TeamPhotos.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import styles from './TeamPhotos.module.css';
+import Modal from 'react-modal';
+
+export const TeamPhotos = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className={styles['modal-wrapper']}>
+      <span onClick={openModal} className={styles['team-modal']}>
+        created by GoIT
+      </span>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Наша команда"
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0.75)',
+          },
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            padding: '20px',
+            borderRadius: '10px',
+            backgroundColor: '#fff',
+            width: '80%',
+            maxWidth: '500px',
+          },
+        }}
+      ></Modal>
+    </div>
+  );
+};
