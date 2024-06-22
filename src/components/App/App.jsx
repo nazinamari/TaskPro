12d4979612a3819291a75ffc190c164d0c3c0d84
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-22 20:37:10 +0100

    fix 401 error

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,48 @@
-import { Suspense, lazy, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { refreshToken } from "../../redux/auth/operations.js";
-import { RestrictedRoute } from "../Routes/RestrictedRoute";
-import { Toaster } from "react-hot-toast";
-import { PrivateRoute } from "../Routes/PrivateRoute";
-import { refreshUser } from "../../redux/user/operations.js";
+import { Suspense, lazy, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { refreshToken } from '../../redux/auth/operations.js';
+import { RestrictedRoute } from '../Routes/RestrictedRoute';
+import { Toaster } from 'react-hot-toast';
+import { PrivateRoute } from '../Routes/PrivateRoute';
+import { refreshUser } from '../../redux/user/operations.js';
+import { selectIsError } from '../../redux/auth/selectors.js';
+import { handleLogOut } from '../../redux/auth/slice.js';
+import { selectIsLoggedIn } from '../../redux/auth/selectors.js';
+import { fetchBoards } from '../../redux/board/operations.js';
 
-const WelcomePage = lazy(() => import("../../pages/WelcomePage/WelcomePage"));
-const AuthPage = lazy(() => import("../../pages/AuthPage"));
-const RegisterForm = lazy(() => import("../RegisterForm/RegisterForm"));
-const LoginForm = lazy(() => import("../LoginForm/LoginForm"));
-const HomePage = lazy(() => import("../../pages/HomePage"));
-const BoardPage = lazy(() => import("../../pages/BoardPage"));
+const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage'));
+const AuthPage = lazy(() => import('../../pages/AuthPage'));
+const RegisterForm = lazy(() => import('../RegisterForm/RegisterForm'));
+const LoginForm = lazy(() => import('../LoginForm/LoginForm'));
+const HomePage = lazy(() => import('../../pages/HomePage'));
+const BoardPage = lazy(() => import('../../pages/BoardPage'));
 const NotFoundPage = lazy(() =>
-  import("../../pages/NotFoundPage/NotFoundPage")
+  import('../../pages/NotFoundPage/NotFoundPage'),
 );
 
 export default function App() {
   const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isError = useSelector(selectIsError);
 
   useEffect(() => {
     dispatch(refreshToken());
-    dispatch(refreshUser());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(refreshUser());
+      dispatch(fetchBoards());
+    }
+  }, [dispatch, isLoggedIn]);
+
+  useEffect(() => {
+    if (isError) {
+      dispatch(handleLogOut());
+      console.error('An error occurred, logging out.');
+    }
+  }, [isError, dispatch]);
 
   return (
     <div>
